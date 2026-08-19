3ff4d74f5559971ee88190c3a371f60459a29899
justovelasquez <justo.ivelasquez@gmail.com>
2026-08-18 20:19:19 -0600

    Actividades Semana 1

---
+++ semana1.eje1/registros.py
@@ -0,0 +1,10 @@
+#autor: Justo velasquez
+#autor: Samuel martinez
+nombre = input("Ingrese su nombre:")
+edad = int(input("Ingrese su edad:"))
+carrera = str(input("Ingrese su carrera:"))
+
+print("tu nombre es " + nombre)
+print("tu edad es " + str(edad))
+print("tu carrera es " + carrera)
+
---
+++ semana1.eje2/operadores.py
@@ -0,0 +1,22 @@
+#autor: Justo velasquez
+#autor: Samuel martinez
+numero1 = int(input("Ingrese su primer numero:"))
+numero2 = int(input("Ingrese su segundo numero: "))
+
+suma = numero1 + numero2
+print("La suma de los numeros es: " + str(suma))
+resta = numero1 - numero2
+print("La resta de los numeros es:" + str(resta))
+multiplicacion = numero1 * numero2
+print("La multiplicacion de los numeros es:" + str(multiplicacion))
+division = numero1 // numero2
+print("La division de los numeros es:" + str(division))
+residuo = numero1 % numero2
+print("El residuo de la division es:" + str(residuo))
+potencia = numero1 ** numero2
+print("La potencia es :" + str(potencia))
+
+
+
+
+
